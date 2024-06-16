00b0655c91a3e13efc3253f1000e50764cf1b18d
Jonathan Jara <jonathan.jara.2000@gmail.com>
2024-06-15 19:02:43 -0700

    starting from scratch

--- src/gui.py
+++
@@ -1,5 +0,0 @@
-import PySimpleGUI as sg
-
-def GUI():
-    Name = 'Unititled'
-    sg.Window(title=Name, layout=[[]], margins=(500, 150)).read()
--- src/setup.py
+++
@@ -1,9 +0,0 @@
-class Setup:
-
-    def __init__(self):
-        pass
-
-    def setup(self):
-        print("Welcome!")
-        
-
